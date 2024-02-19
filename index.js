cb3470d6033134a0cb66673d6739f0efb149eba2
SamreenKhalil <145892400+skillfuldp@users.noreply.github.com>
2024-02-19 22:47:18 +0500

    practice

---
+++ index.js
@@ -0,0 +1,57 @@
+//                  class-Task-2
+
+
+// question 1
+
+// Calculate Total Cost
+//Write a function called calculateTotalCost that takes two arguments: price (the price of an item) and quantity (the quantity of the item purchased). The function should calculate and return the total cost by multiplying the price and quantity. Test the function with different price and quantity values.
+
+// let price = +prompt("enter price");
+// let quantity = +prompt("enter quantity");
+
+// function calculateTotalCost(price, quantity) {
+//     alert( price * quantity);
+// }
+
+// console.log(calculateTotalCost(price , quantity)); 
+
+
+
+// question 2
+
+// Calculate Area of Rectangle
+//Develop a function called calculateArea that accepts two arguments: length and width of a rectangle. The function should calculate and return the area of the rectangle using the formula: area = length * width. Test the function with various length and width values.
+
+// let length = +prompt("enter length");
+// let width = +prompt("enter width");
+
+// function calculateArea(length, width) {
+//     alert( length * width);
+// }
+// console.log(calculateArea(length , width)); 
+
+
+// question 3
+
+// Calculate BMI (Body Mass Index) 
+//Write a function called calculateBMI that takes two arguments: weight (in kilograms) and height (in meters). The function should calculate and return the BMI using the formula: BMI = weight / (height * height). Test the function with different weight and height values.
+
+// let weight = +prompt("enter weight");
+// let height = +prompt("enter height");
+// function calculateBMI(weight, height) {
+//     alert( weight / (height * height));
+// }
+// console.log(calculateBMI(weight , height)); 
+
+
+// question 4
+
+// Calculate Area of Triangle
+//Develop a function named calculateTriangleArea that accepts three arguments: base, height, and side. The function should calculate and return the area of a triangle using the formula: area = (base * height) / 2. Test the function with various base, height, and side values.
+
+// let base = +prompt("enter base");
+// let height = +prompt("enter height");
+// function calculateAOT(base, height) {
+//     alert( (base * height) / 2);
+// }
+// console.log(calculateAOT(base , height)); 
